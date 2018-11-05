d414fef23d99f522f2e714e2cdb45f69b588d39d
AHOF25 <dmitry.shekhovtsov25@gmail.com>
2018-11-05 12:14:50 +0200

    Add project files.

---
+++ Utility/ClassSerialisation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Utility
+{
+   public class ClassSerialisation
+    {
+        public static void SeriliazeToXMl<T>(ref T inObject, string inFileName)
+        {
+            try
+            {
+                XmlSerializer writter = new XmlSerializer(typeof(T));
+                StreamWriter file = new StreamWriter(inFileName);
+                writter.Serialize(file, inObject);
+                file.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public static void DeserializeFromXML<T>(ref T inObject, string inFileName)
+        {
+            if (File.Exists(inFileName))
+            {
+                XmlSerializer reader = new XmlSerializer(typeof(T));
+                StreamReader file = new StreamReader(inFileName);
+                inObject = (T)reader.Deserialize(file);
+                file.Close();
+            }
+        }
+    }
+}
---
+++ Utility/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Utility
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lBFiles = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmiOpenFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(390, 122);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(66, 22);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lBFiles
+            // 
+            this.lBFiles.ContextMenuStrip = this.contextMenuStrip1;
+            this.lBFiles.FormattingEnabled = true;
+            this.lBFiles.Location = new System.Drawing.Point(12, 12);
+            this.lBFiles.Name = "lBFiles";
+            this.lBFiles.Size = new System.Drawing.Size(444, 95);
+            this.lBFiles.TabIndex = 3;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiOpenFile});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(149, 26);
+            // 
+            // tsmiOpenFile
+            // 
+            this.tsmiOpenFile.Name = "tsmiOpenFile";
+            this.tsmiOpenFile.Size = new System.Drawing.Size(148, 22);
+            this.tsmiOpenFile.Text = "Просмотреть";
+            this.tsmiOpenFile.Click += new System.EventHandler(this.tsmiOpenFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 286);
+            this.Controls.Add(this.lBFiles);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.ListBox lBFiles;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiOpenFile;
+    }
+}
+
